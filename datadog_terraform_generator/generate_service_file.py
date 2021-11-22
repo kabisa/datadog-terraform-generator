dfcfc41df9a4db2ffe0bf116c6fc599d0471fddb
Sjuul Janssen <sjuul@outlook.com>
2021-11-22 15:06:42 +0100

    services_file add manual section

--- datadog_terraform_generator/generate_service_file.py
+++ datadog_terraform_generator/generate_service_file.py
@@ -1,14 +1,20 @@
+from os.path import isfile
+
 import yaml
 
 from datadog_terraform_generator.api import DdApi
 from datadog_terraform_generator.config_management import get_config_by_name
 
 
+COMMAND_NAME = "services_file"
+FILE_NAME_ARG = "--file_name"
+
+
 def get_services(dd_api: DdApi, env):
     return dd_api.request(path=f"api/v1/service_dependencies?env={env}")
 
 
-def generate(dd_api, envs, file_name):
+def generate(dd_api, envs, file_name, config_name):
     uncleaned_deps = {}
     for env in envs.split(","):
         services = get_services(dd_api, env=env)
@@ -28,8 +34,20 @@ def generate(dd_api, envs, file_name):
                 dependees[depends_on] = []
             dependees[depends_on].append(service)
 
+    manual_config = {}
+    if isfile(file_name):
+        with open(file_name, "r") as service_calls_fl:
+            existing = yaml.safe_load(service_calls_fl)
+            manual_config = existing.get("manual_configurations", {})
+
     with open(file_name, "w") as service_calls_fl:
-        yaml.safe_dump(service_dependencies, service_calls_fl)
+        service_calls_fl.write(
+            "# This file is generated with the following command:\n"
+            f"# ddtfgen --config {config_name} {COMMAND_NAME} {envs} {FILE_NAME_ARG} {file_name}\n"
+            "# configurations under the manual_configurations section wil remain if regenerated\n"
+        )
+        service_dependencies["manual_configurations"] = manual_config
+        yaml.safe_dump(service_dependencies, service_calls_fl, sort_keys=True)
         print("Written", file_name)
 
 
@@ -39,13 +57,14 @@ def main(args):
         dd_api=DdApi.from_config(config),
         envs=args.envs,
         file_name=args.file_name,
+        config_name=args.config_name,
     )
 
 
 def add_sub_parser(subparsers):
-    parser = subparsers.add_parser("services_file")
+    parser = subparsers.add_parser(COMMAND_NAME)
     parser.add_argument(
         "envs", help="env names you want to be included, separated by a comma"
     )
-    parser.add_argument("--file_name", default="datadog_services.yaml")
+    parser.add_argument(FILE_NAME_ARG, default="datadog_services.yaml")
     parser.set_defaults(func=main)
